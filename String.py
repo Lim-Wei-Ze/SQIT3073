078a36ce2f73c0e65423093b2e29c9eb9b013380
Lim-Wei-Ze <limweize10@gmail.com>
2024-05-09 00:21:53 +0800

    Create String.py

---
+++ String.py
@@ -0,0 +1,36 @@
+# Define a string variable
+text = "Hello, Earth!!!"
+
+# Print the string
+print(text)
+
+# Access individual characters in the string
+first_character = text[0]
+print("The first character is:", first_character)
+
+# Find the length of the string
+length = len(text)
+print("The length of the string is:", length)
+
+# Get user input for the name
+name = input("Please type your name: ")
+
+# Concatenate (combine) two strings
+greeting = "Surprise, " + name + "!"
+print(greeting)
+
+# Use string methods
+uppercase_message = greeting.upper()
+print("Uppercase message:", uppercase_message)
+
+# Check if a substring is in the string
+contains_Earth = "Earth" in greeting
+print("Does the message contain 'Earth'? ", contains_Earth)
+
+# Replace part of the string
+new_message = message.replace("Earth", "World")
+print("Updated message:", new_message)
+
+# Modified for Activity (2 May 24) by LIM WEI ZE (292005)
+# Created by Dr Aamir Adeeb
+# Contact for more information at aamir@uum.edu.my
